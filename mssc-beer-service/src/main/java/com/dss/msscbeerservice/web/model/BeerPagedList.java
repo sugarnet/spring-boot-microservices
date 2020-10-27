81a5b459f7bc730b3cb1cea14b97ae1fc0b069ca
Diego Scifo <diegoscifo@yahoo.com.ar>
2020-10-27 06:44:23 -0300

    Create beer data model and BeerPagedList

---
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/model/BeerPagedList.java
@@ -0,0 +1,16 @@
+package com.dss.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public class BeerPagedList extends PageImpl<BeerDTO> {
+    public BeerPagedList(List<BeerDTO> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPagedList(List<BeerDTO> content) {
+        super(content);
+    }
+}
---
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.dss.msscbeerservice.web.model;
+
+public enum BeerStyleEnum {
+    IPA, STOUT, PILSNER, LAGER, ALE, PORTER;
+}
