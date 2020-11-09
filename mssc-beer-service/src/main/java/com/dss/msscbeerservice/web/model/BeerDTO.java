d66d1a7d66995820883f025f8cd844214b9564c9
Diego Scifo <diegoscifo@yahoo.com.ar>
2020-11-09 08:24:33 -0300

    Add bean validation. Closes #8

--- mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/model/BeerDTO.java
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/model/BeerDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +15,31 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class BeerDTO {
+    @Null
     private UUID id;
+
+    @Null
     private Integer version;
+
+    @Null
     private OffsetDateTime createdDate;
+
+    @Null
     private OffsetDateTime lastModifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private BeerStyleEnum beerStyle;
+
+    @Positive
+    @NotNull
     private Long upc;
+
+    @Positive
+    @NotNull
     private BigDecimal price;
+
     private Integer quantityOnHand;
 }
