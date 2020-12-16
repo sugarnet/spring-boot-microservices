6e2bf0882337e62b3dbf37e71f31634f1ce5416d
Diego Scifo <diegoscifo@yahoo.com.ar>
2020-12-16 07:37:10 -0300

    Add service layer. Closes #15

--- mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/controller/BeerController.java
@@ -1,6 +1,8 @@
 package com.dss.msscbeerservice.web.controller;
 
 import com.dss.msscbeerservice.web.model.BeerDTO;
+import com.dss.msscbeerservice.web.service.BeerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -8,25 +10,25 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
 
+    private final BeerService beerService;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDTO> getBeerById(@PathVariable UUID beerId) {
-        // todo
-        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerService.getById(beerId), HttpStatus.OK);
     }
 
     @PostMapping
     public ResponseEntity<?> createBeer(@RequestBody @Validated BeerDTO beerDTO) {
-        // todo
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        return new ResponseEntity<>(beerService.saveNewBeer(beerDTO), HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
     public ResponseEntity<?> updateBeer(@PathVariable UUID beerId, @RequestBody @Validated BeerDTO beerDTO) {
-        // todo
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(beerService.updateBeer(beerId, beerDTO), HttpStatus.NO_CONTENT);
     }
 }
---
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/service/NotFoundException.java
@@ -0,0 +1,5 @@
+package com.dss.msscbeerservice.web.service;
+
+public class NotFoundException extends RuntimeException {
+
+}
