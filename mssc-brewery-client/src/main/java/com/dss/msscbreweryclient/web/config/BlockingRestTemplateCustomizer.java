b1f5d5ee949c2baeed56091d4406947b5769c818
dscifo <diego.scifo@globallogic.com>
2021-10-28 07:28:24 -0300

    Add http client configuration

---
+++ mssc-brewery-client/src/main/java/com/dss/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,55 @@
+package com.dss.msscbreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    @Value("${http.client.maxTotal}")
+    private int maxTotal;
+
+    @Value("${http.client.defaultMaxPerRoute}")
+    private int defaultMaxPerRoute;
+
+    @Value("${http.client.connectionRequestTimeout}")
+    private int connectionRequestTimeout;
+
+    @Value("${http.client.socketTimeout}")
+    private int socketTimeout;
+
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotal);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
