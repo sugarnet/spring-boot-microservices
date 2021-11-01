f20ac835c4ef3a7168e2258a87900dbc35e899db
dscifo <diego.scifo@globallogic.com>
2021-11-01 05:59:54 -0300

    Add ControllerAdvice and @Valid annotation to controller. Closes #36

--- mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/controller/BeerController.java
+++ mssc-beer-service/src/main/java/com/dss/msscbeerservice/web/controller/BeerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RequestMapping("/api/v1/beer")
@@ -18,13 +19,13 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity<BeerDto> saveNewBeer(@RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> saveNewBeer(@Valid @RequestBody BeerDto beerDto) {
         // todo impl
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity<BeerDto> updateBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity<BeerDto> updateBeer(@PathVariable UUID beerId, @Valid @RequestBody BeerDto beerDto) {
         // todo impl
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
