5edd9ac61c3403e885b1b61f0dc1c63b7711d50e
dscifo <diego.scifo@globallogic.com>
2021-11-24 09:17:47 -0300

    jackson examples

---
+++ mssc-jackson-examples-spring-boot-json-test/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.msscjacksonexamples.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeTest extends BaseTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void snakeTest() throws JsonProcessingException {
+        String json = objectMapper.writeValueAsString(getDto());
+        System.out.println(json);
+    }
+}
