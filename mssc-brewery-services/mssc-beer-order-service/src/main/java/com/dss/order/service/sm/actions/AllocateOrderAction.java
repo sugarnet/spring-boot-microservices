2f8eaf8cf362111a50426edd3df074c05557c2e5
dscifo <diego.scifo@globallogic.com>
2022-03-03 07:20:14 -0300

    mssc-beer-service: mssc-beer-order-service: config state machine for allocate order. mssc-beer-inventory-service:
    Closes #87

--- mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/sm/actions/AllocateOrderAction.java
+++ mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/sm/actions/AllocateOrderAction.java
@@ -14,6 +14,7 @@ import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.action.Action;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
@@ -29,9 +30,12 @@ public class AllocateOrderAction implements Action<BeerOrderStatusEnum, BeerOrde
     public void execute(StateContext<BeerOrderStatusEnum, BeerOrderEventEnum> stateContext) {
 
         String beerOrderId = (String) stateContext.getMessage().getHeaders().get(BeerOrderManagerImpl.ORDER_ID_HEADER);
-        BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
 
-        jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_QUEUE, beerOrderMapper.beerOrderToDto(beerOrder));
+        if (Objects.nonNull(beerOrderId)) {
+            BeerOrder beerOrder = beerOrderRepository.findOneById(UUID.fromString(beerOrderId));
+
+            jmsTemplate.convertAndSend(JmsConfig.ALLOCATE_ORDER_QUEUE, beerOrderMapper.beerOrderToDto(beerOrder));
+        }
 
         log.debug("Sent allocation request for order id {}", beerOrderId);
 
