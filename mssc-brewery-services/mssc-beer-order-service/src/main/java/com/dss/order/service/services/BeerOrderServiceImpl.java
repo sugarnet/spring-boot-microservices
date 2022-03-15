20648aeff1ff11e1be125cc22c705a03c02e223a
dscifo <diego.scifo@globallogic.com>
2022-03-15 07:34:18 -0300

    mssc-beer-service: mssc-beer-order-service: refactor BeerOrderServiceImpl using BeerOrderManager mssc-beer-inventory-service:
    closes #92

--- mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/services/BeerOrderServiceImpl.java
+++ mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/services/BeerOrderServiceImpl.java
@@ -100,17 +100,17 @@ public class BeerOrderServiceImpl implements BeerOrderService {
         beerOrderManager.beerOrderPickedUp(orderId);
     }
 
-    private BeerOrder getOrder(UUID customerId, UUID orderId){
+    private BeerOrder getOrder(UUID customerId, UUID orderId) {
         Optional<Customer> customerOptional = customerRepository.findById(customerId);
 
-        if(customerOptional.isPresent()){
+        if (customerOptional.isPresent()) {
             Optional<BeerOrder> beerOrderOptional = beerOrderRepository.findById(orderId);
 
-            if(beerOrderOptional.isPresent()){
+            if (beerOrderOptional.isPresent()) {
                 BeerOrder beerOrder = beerOrderOptional.get();
 
                 // fall to exception if customer id's do not match - order not for customer
-                if(beerOrder.getCustomer().getId().equals(customerId)){
+                if (beerOrder.getCustomer().getId().equals(customerId)) {
                     return beerOrder;
                 }
             }
