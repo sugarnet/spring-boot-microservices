efc5a0f3174de17f31fca9775723968a3696aa1c
dscifo <diego.scifo@globallogic.com>
2022-03-23 09:17:31 -0300

    mssc-beer-service: mssc-beer-order-service: mssc-beer-inventory-service: mssc-brewery-gateway: add proxy google example
    closes #103

---
+++ mssc-brewery-services/mssc-brewery-gateway/src/main/java/com/dss/gateway/config/GoogleConfig.java
@@ -0,0 +1,23 @@
+package com.dss.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("google")
+@Configuration
+public class GoogleConfig {
+
+    @Bean
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/search")
+                        .filters(f -> f.rewritePath("/search2(?<segment>/?.*)", "/${segment}"))
+                        .uri("https://www.google.com"))
+                .build();
+    }
+
+}
