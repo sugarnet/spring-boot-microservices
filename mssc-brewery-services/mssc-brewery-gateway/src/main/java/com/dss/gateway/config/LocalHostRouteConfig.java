1447bc207db9b93bdea60122867f353856f9d340
dscifo <diego.scifo@globallogic.com>
2022-03-25 07:19:35 -0300

    mssc-beer-service: mssc-beer-order-service: mssc-beer-inventory-service: mssc-brewery-gateway: add config for beer service
    closes #106

---
+++ mssc-brewery-services/mssc-brewery-gateway/src/main/java/com/dss/gateway/config/LocalHostRouteConfig.java
@@ -0,0 +1,18 @@
+package com.dss.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LocalHostRouteConfig {
+    @Bean
+    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beer/upc/*")
+                        .uri("http://localhost:8080"))
+                .build();
+    }
+}
