96087c99e7e00403f5bf0cc8a2856646ef784696
dscifo <diego.scifo@globallogic.com>
2022-03-25 06:27:16 -0300

    mssc-beer-service: mssc-beer-order-service: fix tasting room problems mssc-beer-inventory-service: mssc-brewery-gateway:
    closes #105

--- mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/bootstrap/BeerOrderBootStrap.java
+++ mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/bootstrap/BeerOrderBootStrap.java
@@ -29,7 +29,7 @@ public class BeerOrderBootStrap implements CommandLineRunner {
     }
 
     private void loadCustomerData() {
-        if (customerRepository.count() == 0) {
+        if (customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM).size() == 0) {
             Customer customer = customerRepository.save(Customer.builder()
                     .customerName(TASTING_ROOM)
                     .apiKey(UUID.randomUUID())
--- mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/services/TastingRoomService.java
+++ mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/services/TastingRoomService.java
@@ -7,6 +7,7 @@ import com.dss.order.service.domain.Customer;
 import com.dss.order.service.repositories.BeerOrderRepository;
 import com.dss.order.service.repositories.CustomerRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,7 +37,7 @@ public class TastingRoomService {
     }
 
     @Transactional
-    //@Scheduled(fixedRate = 2000) //run every 2 seconds
+    @Scheduled(fixedRate = 2000) //run every 2 seconds
     public void placeTastingRoomOrder(){
 
         List<Customer> customerList = customerRepository.findAllByCustomerNameLike(BeerOrderBootStrap.TASTING_ROOM);
@@ -45,6 +46,8 @@ public class TastingRoomService {
             doPlaceOrder(customerList.get(0));
         } else {
             log.error("Too many or too few tasting room customers found");
+
+            customerList.forEach(customer -> log.debug(customer.toString()));
         }
     }
 
