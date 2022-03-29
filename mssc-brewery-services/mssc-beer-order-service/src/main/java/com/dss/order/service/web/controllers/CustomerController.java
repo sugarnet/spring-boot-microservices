427e5ced05603865556b536d1a46cbf7e424e92e
dscifo <diego.scifo@globallogic.com>
2022-03-29 07:03:16 -0300

    mssc-beer-service: mssc-beer-order-service: fix problem in CustomerController mssc-beer-inventory-service: mssc-brewery-gateway:
    closes #108

--- mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/web/controllers/CustomerController.java
+++ mssc-brewery-services/mssc-beer-order-service/src/main/java/com/dss/order/service/web/controllers/CustomerController.java
@@ -5,6 +5,7 @@ import com.dss.order.service.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    @GetMapping
     public CustomerPagedList listCustomers(@RequestParam(value = "pageNumber", required = false) Integer pageNumber, @RequestParam(value = "pageSize", required = false) Integer pageSize) {
         if (pageNumber == null || pageNumber < 0) {
             pageNumber = DEFAULT_PAGE_NUMBER;
