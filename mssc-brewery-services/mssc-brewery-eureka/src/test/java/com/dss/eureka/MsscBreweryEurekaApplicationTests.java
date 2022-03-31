a67f4bb5f5d382e11d6f44b9ae913164063d3bcd
dscifo <diego.scifo@globallogic.com>
2022-03-31 06:38:03 -0300

    mssc-beer-service: mssc-beer-order-service: mssc-beer-inventory-service: mssc-brewery-gateway: mssc-brewery-eureka: initial project
    closes #110

---
+++ mssc-brewery-services/mssc-brewery-eureka/src/test/java/com/dss/eureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
