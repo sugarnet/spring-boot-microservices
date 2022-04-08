567d5740b820eda653d791736e45143236a9b4cd
dscifo <diego.scifo@globallogic.com>
2022-04-08 07:19:12 -0300

    mssc-beer-service: mssc-beer-order-service: mssc-beer-inventory-service: mssc-beer-inventory-failover: add project mssc-brewery-gateway: mssc-brewery-eureka:
    closes #116

---
+++ mssc-brewery-services/mssc-beer-inventory-failover/src/main/java/com/dss/inventoryfailover/web/InventoryHandler.java
@@ -0,0 +1,28 @@
+package com.dss.inventoryfailover.web;
+
+import com.dss.inventoryfailover.model.BeerInventoryDto;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class InventoryHandler {
+
+    public Mono<ServerResponse> listInventory(ServerRequest serverRequest) {
+        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON)
+                .body(Arrays.asList(BeerInventoryDto.builder()
+                        .id(UUID.randomUUID())
+                        .createdDate(OffsetDateTime.now())
+                        .lastModifiedDate(OffsetDateTime.now())
+                        .beerId(UUID.fromString("0000000-0000-0000-0000-000000000000"))
+                        .quantityOnHand(999)
+                        .build()), List.class);
+    }
+}
---
+++ mssc-brewery-services/mssc-beer-inventory-failover/src/test/java/com/dss/inventoryfailover/MsscBeerInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.inventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
