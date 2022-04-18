3a5a52e26d91f7d251921edb690a92dfdb0f6379
dscifo <diego.scifo@globallogic.com>
2022-04-18 06:32:42 -0300

    mssc-beer-service: mssc-beer-order-service: mssc-beer-inventory-service: mssc-beer-inventory-failover: mssc-brewery-gateway: mssc-brewery-eureka: mssc-config-server: add project
    closes #119

---
+++ mssc-brewery-services/mssc-config-server/src/test/java/com/dss/config/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dss.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
